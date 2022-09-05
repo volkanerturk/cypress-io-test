8fb90fe3711b733baf5417cae79a5e9684335d1f
Volkan <volkan@Volkans-MacBook-Pro.local>
2022-09-05 17:06:11 +0300

    Change test desribtion

--- cypress/e2e/visitFb.cy.js
+++ cypress/e2e/visitFb.cy.js
@@ -1,8 +1,8 @@
-context("Login Test", () => {
+context("Visit Facebook", () => {
     beforeEach(() => {
         cy.visit("https://www.facebook.com");
     });
-    it("Click login buton and send user login form", () => {
+    it("Sendkeys username", () => {
         cy.get('[data-testid="royal_email"]').type("123456");
         cy.get('[data-testid="royal_email"]').clear();
 });
