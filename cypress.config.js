5eaf559e8888759677569440efcf0196f70857ed
Volkan <volkan@Volkans-MacBook-Pro.local>
2022-09-05 16:27:14 +0300

    clear input

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 't143h8',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/visitFb.cy.js
+++ cypress/e2e/visitFb.cy.js
@@ -4,5 +4,6 @@ context("Login Test", () => {
     });
     it("Click login buton and send user login form", () => {
         cy.get('[data-testid="royal_email"]').type("123456");
+        cy.get('[data-testid="royal_email"]').clear();
 });
 });
