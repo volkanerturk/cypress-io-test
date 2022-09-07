082fd2f67f8bca634c54a4d6fa4ba45f802e3b8d
Volkan <volkan@Volkans-MacBook-Pro.local>
2022-09-07 11:23:52 +0300

    Fix url

--- cypress/e2e/tatilBudur.cy.js
+++ cypress/e2e/tatilBudur.cy.js
@@ -2,8 +2,8 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
   });
 context('Switch tab test', function () {
-    it("Visit tatil budur'", () => {
-        cy.visit('https://www.tatilbudur.com/yurtici-oteller');
+    it("Visit tatil budur", () => {
+        cy.visit('https://tatilbudur.com/yurtici-oteller');
     });
     it('Invoke delete and click hotel', function () {
         cy.contains("Siam Elegance Hotels Spa")
