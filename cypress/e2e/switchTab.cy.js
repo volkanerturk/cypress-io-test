c129532fcf2b28bbae859cd38f437d3db531526b
Volkan <volkan@Volkans-MacBook-Pro.local>
2022-09-09 15:50:17 +0300

    try new fix

--- cypress/e2e/switchTab.cy.js
+++ cypress/e2e/switchTab.cy.js
@@ -1,5 +1,11 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
+    // we expect a Tauri error about the window
+    // and don't want to fail the test so we return false
+    if (err.message.includes('window.__TAURI_IPC__ is not a function')) {
+      return false;
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
   });
 context.only('Switch tab test', function () {
     it("Visit tatil budur", () => {
