e862d881ab403aecbc1f53ce8d9bcdfe98f33494
Volkan <volkan@Volkans-MacBook-Pro.local>
2022-09-16 15:41:54 +0300

    fix

--- cypress/e2e/switch-tab.cy.js
+++ cypress/e2e/switch-tab.cy.js
@@ -1,10 +1,10 @@
 context("Switch tab test", () => {  
-        it("Example 1 Failed ", () => {
+        /*it("Example 1 Failed ", () => {
         cy.visit("https://the-internet.herokuapp.com/windows")
         cy.get(".example > a")
         .click();
         cy.get("h3").should("have.text", "New Window")   
-    });
+    });*/
     it("Example 2 Passed", () => {
         cy.visit("https://the-internet.herokuapp.com/windows")
         cy.get(".example > a")
